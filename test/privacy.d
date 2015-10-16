54481dc6c8bbe487550e6ddc4478a82e51ff008d
Ryan Roden-Corrent <ryan@rcorre.net>
2015-10-16 14:47:01 -0400

    Test that SuperStruct respects access modifiers.
    SuperStruct should not expose a member if it is non-public on at least
    one of the source types.

---
+++ test/privacy.d
@@ -0,0 +1,35 @@
+module privacy;
+
+import superstruct;
+
+// public/public:
+unittest {
+  struct A { int i; }
+  struct B { int i; }
+  SuperStruct!(A, B) ab;
+  static assert(__traits(compiles, { auto i = ab.i; } ));
+}
+
+// public/private
+unittest {
+  struct A { int i; }
+  struct B { private int i; }
+  SuperStruct!(A, B) ab;
+  static assert(!__traits(compiles, { auto i = ab.i; } ));
+}
+
+// package/private
+unittest {
+  struct A { int i; }
+  struct B { package int i; }
+  SuperStruct!(A, B) ab;
+  static assert(!__traits(compiles, { auto i = ab.i; } ));
+}
+
+// private/private
+unittest {
+  struct A { private int i; }
+  struct B { private int i; }
+  SuperStruct!(A, B) ab;
+  static assert(!__traits(compiles, { auto i = ab.i; } ));
+}
